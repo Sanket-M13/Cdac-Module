4ba7b53e148601b61ff49e596ddee066fadfa9d6
Sanket Mandavgane <redgear673@gmail.com>
2025-09-04 18:59:04 +0530

    Assignment 3

---
+++ OOPJ/Assignment 3/BankAccount.java
@@ -0,0 +1,37 @@
+class BankAccount{
+	private double balance;
+	
+	BankAccount(){}
+	
+	void deposite(double amt){
+		this.balance += amt;
+		System.out.println("Balance Deposite");
+		System.out.println("Your Current balance is: "+ this.balance);
+	}
+	
+	void withdraw(double amt){
+		if(amt > balance){
+			System.out.println("Not sufficient Balance");
+		}else{
+			this.balance -= amt;
+			System.out.println("Withdrawl Successfull");
+		}
+	}
+	
+	void getBalance(){
+		System.out.println("Your balance is: "+ this.balance);
+	}
+	
+	public static void main(String args[]){
+	
+		BankAccount B1 = new BankAccount();
+		B1.deposite(45500);
+		B1.withdraw(25000);
+		B1.getBalance();
+		
+		
+	}
+	
+	
+	
+}
