f04c3140efe15feb58f1cbd1adc6b6b73819216b
Sanket Mandavgane <redgear673@gmail.com>
2025-09-04 22:08:33 +0530

    Assignment 3

---
+++ OOPJ/Assignment 3/Main.java
@@ -0,0 +1,47 @@
+abstract class Shape{
+	abstract double area();
+}
+
+class Rectangle extends Shape{
+
+	double length,breadth;
+	
+	Rectangle(double len, double breadth){
+		this.length = len;
+		this.breadth = breadth;
+	}
+	
+	@Override
+	double area(){
+		return length * breadth;
+	}
+}
+
+class Circle extends Shape{
+	double radius;
+	
+	Circle(double radius) {
+        this.radius = radius;
+    }
+	
+	@Override
+	double area(){
+		return 3.14 * (radius * radius);
+	}
+	
+}
+
+class Main{
+	public static void main(String args[]){
+		Rectangle r1 = new Rectangle(5,10);
+		
+		System.out.println("Rectangle Area: "+ r1.area());
+		
+		Circle c1 = new Circle(7);
+		
+		System.out.println("Circle Area: "+ c1.area());
+		
+		
+	}
+	
+}
