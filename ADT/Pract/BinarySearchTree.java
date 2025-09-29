2f8d361a046ae5284e29bd0fcc2528294d5f7cf5
Sanket Mandavgane <redgear673@gmail.com>
2025-09-29 08:25:57 +0530

    Ads

--- ADT/Pract/BinarySearchTree.java
+++ ADT/Pract/BinarySearchTree.java
@@ -38,6 +38,37 @@ class BinarySearchTree{
         return root;
     }
 
+    void delete(int key){
+        root = deletedata(root,key);
+    }
+
+    Node deletedata(Node root, int key){
+        if(root == null)
+            return root;
+        
+        if(key<root.data){
+            root.left = deletedata(root.left,key);
+        }else if(key>root.data){
+            root.right = deletedata(root.right, key);
+        }else{
+            if(root.left == null)
+                return root.right;
+            if(root.right == null)
+                return root.left;
+            root.data = minvalue(root.right);
+            root.right = deletedata(root.right,root.data);
+        }
+        return root;
+    }
+    int minvalue(Node n){
+        int x = root.data;
+        while(root.left != null ){
+            x = root.left.data;
+            root = root.left;
+        }
+        return x;
+    }
+
     public static void main(String args[]){
         BinarySearchTree bs = new BinarySearchTree();
         bs.insert(45);
@@ -48,6 +79,9 @@ class BinarySearchTree{
         bs.insert(50);
         bs.insert(41);
 
+        bs.inorder(bs.root);
+        bs.delete(44);
+        System.out.println();
         bs.inorder(bs.root);
 
 
--- ADT/Pract/Bsort.java
+++ ADT/Pract/Bsort.java
@@ -1,25 +1,20 @@
 class Bsort{
 	void bSort(int arr[]){
 		int n = arr.length;
-		int c = 0;
-		// boolean x;
+	
 		for(int i = 0; i<n-1; i++){
-			// x = false;
-			c++;
+		
 			for(int j = 0; j<n-i-1; j++){
 				if(arr[j] > arr[j+1]){
 					int temp = arr[j];
 					arr[j] = arr[j+1];
-					arr[j+1] = temp;
-					c++;
-					// x = true;
+					arr[j+1] = temp;	
 				}
 			}
 			// if (!x) {
                 // break;
             // }
 		}
-		System.out.println(c);
 	}
 	
 	void display(int arr[]){
