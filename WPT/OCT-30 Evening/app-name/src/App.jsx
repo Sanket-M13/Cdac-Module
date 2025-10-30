977f1bcdb734e5f491680d06f37c5c596494bb92
Sanket Mandavgane <redgear673@gmail.com>
2025-10-30 20:11:46 +0530

    wpt

---
+++ WPT/OCT-30 Evening/app-name/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hellow world</h1>
+      <h1>Hiii</h1>
+      </div>
+  )
+}
+
+export default App
---
+++ WPT/OCT-30 Evening/app-name/src/demo.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+
+class Demo extends Component {
+    
+    render(){
+        
+    }
+}
